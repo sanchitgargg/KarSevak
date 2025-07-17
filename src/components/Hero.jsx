36791fc26e48901d16b55e3722ff0b7ad123bfd0
sanchitgargg <gargsanchit108@gmail.com>
2025-07-17 21:02:56 +0530

    Header Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='relative h-screen overflow-hidden flex items-center justify-center'>
-        <img src="/hero.jpg" className='absolute inset-0 w-full h-full object-cover rounded-3xl px-8 py-5 z-0' />
+        <img src="/hero.png" className='absolute inset-0 w-full h-full object-cover z-0' />
         <div className='relative z-10 flex flex-col items-center justify-center'>
         <p className='text-5xl text-white font-bold'>Join Hands - Serve Together</p>
         <p className='text-lg text-white'>Connect with your community and make a difference through meaningful volunteer opportunities</p>
